8651f88e6d4026e3b7377f6ca7f2b86c05a15e3f
AtulVerma_up45 <AtulVerma04680@gmail.com>
2024-06-20 09:37:41 +0530

    basic setup, folder structure and some styling of root page

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Spotify Clone - Web Player: Music for everyone",
+  description:
+    "Spotify Clone is a digital music service that gives you access to millions of songs. Which is Crafted by Atul Verma",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
+    </html>
+  );
+}
